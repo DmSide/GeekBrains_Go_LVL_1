f2035216835b7d2cfa1ec7bf2886ed248d75ad6f
DmSide <dmsidenko@gmail.com>
2020-12-19 22:24:35 +0300

    TASK_6 Add custom interfaces

--- lesson1/hello_world.go
+++ lesson1/hello_world.go
@@ -1,14 +1,20 @@
 package lesson1
 
-func HelloWorld(country string) string {
+type HelloWorldError struct{}
+
+func (*HelloWorldError) Error() string {
+	return "I don't know the entered country. Country must contain only 2 letters"
+}
+
+func HelloWorld(country string) (string, error) {
 	switch country {
 	case "RU":
-		return "Привет, мир!"
+		return "Привет, мир!", nil
 	case "EN":
-		return "Hello world!"
+		return "Hello world!", nil
 	case "ZN":
-		return "你好，世界!"
+		return "你好，世界!", nil
 	default:
-		return "I don't know the entered country. Country must contain only 2 letters"
+		return "", &HelloWorldError{}
 	}
 }
